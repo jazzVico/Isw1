dbce995417300fd61cd79748d24b748e52ce1c9a
Jazmín Vico <jazminalvarezvico@gmail.com>
2019-08-29 23:01:39 -0300

    comienzo tp1

---
+++ TP1/tp1.py
@@ -0,0 +1,16 @@
+class Suelo:
+	def __init__(self, dureza, porosidad):
+    self.Dureza=dureza
+	self.Porosidad=porosidad
+
+
+class BrazoEscabador:
+	def __init__(self):
+
+	public:
+		def Escabar(suelo):
+	private:
+		def EscabarSueloDuro()
+		def EscabarSueloBlando()
+		def GirarMecha(sentido,velocidad,tiempo)
+		def CerrarPinza()
